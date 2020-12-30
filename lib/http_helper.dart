cf75788a6234db7054edac6c83f0ebad845313aa
Ankoor Patel <ankoor.p@gmail.com>
2020-12-30 23:11:40 +0000

    pg.184 search functionality working

--- lib/http_helper.dart
+++ lib/http_helper.dart
@@ -7,26 +7,49 @@ import 'movie.dart';
 
 class HttpHelper {
   final String apiKey = '2361ca3aec42d11fbd7d2a27a5f46851';
-  Map<String, String> queryParameters;
+  Map<String, String> basicQueryParameters;
+  final String scheme = 'https';
   final String host = 'api.themoviedb.org';
   Uri upcomingUri;
 
   HttpHelper() {
-    queryParameters = {
+    basicQueryParameters = {
       'api_key': apiKey,
       'language': 'en-US',
     };
 
     upcomingUri = Uri(
-      scheme: 'https',
+      scheme: scheme,
       host: host,
       pathSegments: ['3', 'movie', 'upcoming'],
-      queryParameters: queryParameters,
+      queryParameters: basicQueryParameters,
     );
   }
 
+  Uri buildSearchUri(String query) {
+    Map<String, String> searchQueryParameters = {
+      'query': query,
+    };
+    searchQueryParameters.addAll(basicQueryParameters);
+
+    return Uri(
+      scheme: scheme,
+      host: host,
+      pathSegments: ['3', 'search', 'movie'],
+      queryParameters: searchQueryParameters,
+    );
+  }
+
+  Future<List<Movie>> findMovies(String title) async {
+    return queryMovieApi(buildSearchUri(title));
+  }
+
   Future<List<Movie>> getUpcoming() async {
-    http.Response result = await http.get(upcomingUri.toString());
+    return queryMovieApi(upcomingUri);
+  }
+
+  Future<List<Movie>> queryMovieApi(Uri uri) async {
+    http.Response result = await http.get(uri.toString());
 
     if (result.statusCode == HttpStatus.ok) {
       final jsonResponse = json.decode(result.body);
--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -15,17 +15,20 @@ class _MovieListState extends State<MovieList> {
   List<Movie> movies;
   int movieCount;
   HttpHelper helper;
-
+  final Icon searchIcon = Icon(Icons.search);
+  final Icon cancelIcon = Icon(Icons.cancel);
+  Icon visibleIcon;
   final String iconBaseUrl = 'https://image.tmdb.org/t/p/w92/';
 
   @override
   void initState() {
     helper = HttpHelper();
-    initialize();
+    loadUpcomingMovies();
+    visibleIcon = searchIcon;
     super.initState();
   }
 
-  Future initialize() async {
+  Future loadUpcomingMovies() async {
     List<Movie> upComingMovies = List();
     upComingMovies = await helper.getUpcoming();
     setState(() {
@@ -37,11 +40,44 @@ class _MovieListState extends State<MovieList> {
   StatelessWidget posterIcon(String src) =>
       CircleAvatar(backgroundImage: NetworkImage(src));
 
+  Widget searchBar = Text('Movies');
+
+  Future submitSearch(String text) async {
+    final List<Movie> foundMovies = await helper.findMovies(text);
+    setState(() {
+      movieCount = foundMovies.length;
+      movies = foundMovies;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie List'),
+        title: searchBar,
+          actions: <Widget>[
+            IconButton(icon: visibleIcon, onPressed: () {
+              if (visibleIcon.icon == Icons.search) {
+                setState(() {
+                  visibleIcon = cancelIcon;
+                  searchBar = TextField(
+                    textInputAction: TextInputAction.search,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                    autofocus: true,
+                    showCursor: true,
+                    onSubmitted: submitSearch,
+                  );
+                });
+              } else {
+                visibleIcon = searchIcon;
+                searchBar = Text('Movies');
+                loadUpcomingMovies();
+              }
+            }),
+          ],
       ),
       body: ListView.builder(
         itemCount: (this.movieCount == null) ? 0 : this.movieCount,
